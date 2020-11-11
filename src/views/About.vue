0e1bc96667288c5eea4cc4255379c97ea41e1def
Morgan Benton <morgan.benton@gmail.com>
2020-11-11 09:32:28 -0500

    add vuetify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p class="red">Here is some content.</p>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
